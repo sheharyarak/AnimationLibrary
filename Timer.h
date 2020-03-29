8de7922213ba34ec421222f6931228c12b0b8836
sheharyarak <31715063+sheharyarak@users.noreply.github.com>
2020-03-29 04:01:43 -0700

    added timer

---
+++ Timer.h
@@ -0,0 +1,24 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <chrono>
+#include <iostream>
+// #include <thread>
+
+struct Timer{
+	std::string name;
+	std::chrono::system_clock::time_point start, end;
+	std::chrono::duration<float> duration;
+	Timer(std::string name){
+		this->name = name;
+		start = std::chrono::high_resolution_clock::now();
+	}
+	~Timer()
+	{
+		end = std::chrono::high_resolution_clock::now();
+		duration = end - start;
+		std::cout << name << " : " << duration.count() << "s." <<std::endl;
+	}
+};
+
+#endif
